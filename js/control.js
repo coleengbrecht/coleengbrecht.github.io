d1c4b2ee534abdca6164b1d18021c942a7986730
Coleman Engbrecht <linuxFTW49@gmail.com>
2019-05-18 07:50:51 -0600

    Am now using bootstrap Content update

--- js/control.js
+++ js/control.js
@@ -1,14 +1,34 @@
 
-const pageCount = 3;
-
-document.onload = function() {
+window.onload = function() {
+	$('iframe').each(function(index,item) {
+		if($(item).attr('src').match(/(https?:)?\/\/www\.youtube\.com/)) {
+			var w=$(item).attr('width');
+			var h=$(item).attr('height');
+			var ar = h/w*100;
+			ar=ar.toFixed(2);
+			//Style iframe
+			$(item).css('position','absolute');
+			$(item).css('top','0');
+			$(item).css('left','0');
+			$(item).css('width','100%');
+			$(item).css('height','100%');
+			$(item).css('max-width',w+'px');
+			$(item).css('max-height', h+'px');
+			$(item).wrap('<div style="max-width:'+w+'px;margin:0 auto; padding:auto;" />');
+			$(item).wrap('<div style="position: relative;padding-bottom: '+ar+'%; height: 0; overflow: hidden;" />');
+		}
+	});
 }
 
 show_page = function(page) {
-	for (let i = 0; i < pageCount; ++i) {
-		let elem = document.getElementById("page-" + i);
-		elem.style.display = "none";
+	let links = document.getElementsByClassName("nav-link");
+	let pages = document.getElementsByClassName("page");
+	if (page < pages.length) {
+		for (let i = 0; i < pages.length; ++i) {
+			pages[i].style.display = "none";
+			links[i].className = "nav-link ";
+		}
+		pages[page].style.display = "block";
+		links[page].className += "active";
 	}
-	let elem = document.getElementById("page-" + page);
-	elem.style.display = "block";
 }
